29241c742da8468bd47d7ebde6f0fea0ecd061c5
Thanya Moolmuang <tmoolmuang@yahoo.com>
2018-04-03 15:27:02 -0700

    remove hapi server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Path = require('path');
 
 let connection = {
     port: process.env.PORT || 3000,
-    host: process.env.IP || 'localhost'
+    //host: process.env.IP || 'localhost'
 };
 
 let routes = [
